3042fb8f526ad57a3f1bce49b4cf355028a51923
cyxw <cyxw6214@foxmail.com>
2022-09-03 14:48:26 +0800

    optimize

--- js/classes/player.js
+++ js/classes/player.js
@@ -159,7 +159,14 @@ class Player {
                 for (let layer in player.layers) {
                     let intlayer = parseInt(layer)
                     if (player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].ascensionAvailable()) {
+                        if(player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].parent_layer != undefined)
+                        {
+                            player.layers[player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].parent_layer.id].buyLeft();
+                            player.layers[player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].parent_layer.id].buyRight();
+                        }
+                        if (player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].canPrestige())
                         player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].prestige();
+                        else return;
                         player.last_auto_id_asc = (intlayer + player.last_auto_id_asc) % player.layers.length;
                         break;
                     }
@@ -174,7 +181,14 @@ class Player {
                 for (let layer in player.layers) {
                     let intlayer = parseInt(layer)
                     if (player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].calculateProduction().lte(0) && player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].canPrestige()) {
+                        if(player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].parent_layer != undefined)
+                        {
+                            player.layers[player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].parent_layer.id].buyLeft();
+                            player.layers[player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].parent_layer.id].buyRight();
+                        }
+                        if (player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].canPrestige())
                         player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].prestige();
+                        else return;
                         player.last_auto_id_zero = (intlayer + player.last_auto_id_zero) % player.layers.length;
                         break;
                     }
@@ -189,7 +203,14 @@ class Player {
                 for (let layer in player.layers) {
                     let intlayer = parseInt(layer)
                     if (player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].points.times(player.AutoAscension_More.multi).lt(player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].prestigeGain()) && !player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].right_branch) {
+                        if(player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].parent_layer != undefined)
+                        {
+                            player.layers[player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].parent_layer.id].buyLeft();
+                            player.layers[player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].parent_layer.id].buyRight();
+                        }
+                        if (player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].canPrestige())
                         player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].prestige();
+                        else return;
                         player.last_auto_id_more = (intlayer + player.last_auto_id_more) % player.layers.length;
                         break;
                     }
--- js/main.js
+++ js/main.js
@@ -139,7 +139,14 @@ document.getElementById("autoascension-toggle").addEventListener("click",() => {
             for (let layer in player.layers) {
                 let intlayer = parseInt(layer)
                 if (player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].ascensionAvailable()) {
+                    if(player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].parent_layer != undefined)
+                    {
+                        player.layers[player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].parent_layer.id].buyLeft();
+                        player.layers[player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].parent_layer.id].buyRight();
+                    }
+                    if (player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].canPrestige())
                     player.layers[(intlayer + player.last_auto_id_asc) % player.layers.length].prestige();
+                    else return;
                     player.last_auto_id_asc = (intlayer + player.last_auto_id_asc) % player.layers.length;
                     break;
                 }
@@ -176,7 +183,14 @@ document.getElementById("autozero-toggle").addEventListener("click",() => {
             for (let layer in player.layers) {
                 let intlayer = parseInt(layer)
                 if (player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].calculateProduction().lte(0) && player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].canPrestige()) {
+                    if(player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].parent_layer != undefined)
+                    {
+                        player.layers[player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].parent_layer.id].buyLeft();
+                        player.layers[player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].parent_layer.id].buyRight();
+                    }
+                    if (player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].canPrestige())
                     player.layers[(intlayer + player.last_auto_id_zero) % player.layers.length].prestige();
+                    else return;
                     player.last_auto_id_zero = (intlayer + player.last_auto_id_zero) % player.layers.length;
                     break;
                 }
@@ -217,7 +231,14 @@ document.getElementById("automore-toggle").addEventListener("click",() => {
             for (let layer in player.layers) {
                 let intlayer = parseInt(layer)
                 if (player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].points.times(player.AutoAscension_More.multi).lt(player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].prestigeGain()) && !player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].right_branch) {
+                    if(player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].parent_layer != undefined)
+                    {
+                        player.layers[player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].parent_layer.id].buyLeft();
+                        player.layers[player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].parent_layer.id].buyRight();
+                    }
+                    if (player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].canPrestige())
                     player.layers[(intlayer + player.last_auto_id_more) % player.layers.length].prestige();
+                    else return;
                     player.last_auto_id_more = (intlayer + player.last_auto_id_more) % player.layers.length;
                     break;
                 }
